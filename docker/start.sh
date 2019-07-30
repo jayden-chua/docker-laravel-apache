d4ed0f8b9932df58d742882e56aeb7ec9968733d
Jayden Chua <jayden.chua@modix.cn>
2019-07-30 14:33:34 +0800

    opcache with apache

--- docker/start.sh
+++ docker/start.sh
@@ -3,10 +3,35 @@
 set -e
 
 env=${APP_ENV:-production}
+role=${CONTAINER_ROLE:-app}
 
 if [ "$env" != "local" ]; then
+    (
+        cd /var/www/html &&
+        php artisan config:cache &&
+        php artisan route:cache &&
+        php artisan view:cache
+    )
     echo "removing Xdebug"
     rm -rf /usr/local/etc/php/conf.d/{docker-php-ext-xdebug.ini,xdebug.ini}
 fi
 
-exec apache2-foreground
+echo "The role $role"
+
+if [ "$role" = "app" ]; then
+    exec apache2-foreground
+elif [ "$role" = "scheduler" ]; then
+    while [ true ]
+    do 
+        php /var/www/html/artisan schedule:run --verbose --no-interaction &
+        sleep 60
+    done
+elif [ "$role" = "queue" ]; then 
+    echo "Running the queue"
+    exec php /var/www/html/artisan queue:work --verbose --tries=3
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
+
+

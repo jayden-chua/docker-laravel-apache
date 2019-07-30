09d5c05605d2ea62fc1b9b3335684635cc65f00b
Jayden Chua <jayden.chua@modix.cn>
2019-07-30 10:27:28 +0800

    add start script to remove xdebug

---
+++ docker/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+env=${APP_ENV:-production}
+
+if [ "$env" != "local" ]; then
+    echo "removing Xdebug"
+    rm -rf /usr/local/etc/php/conf.d/{docker-php-ext-xdebug.ini,xdebug.ini}
+fi
+
+exec apache2-foreground
